8c34b4debbfbe92a0a0169f7d06ea828d9e73165
Harshvardhan Sharma <vardhan.harsh23aug@gmail.com>
2025-07-27 10:18:30 +0000

    Revert "TASK001: Consumer Controller Threads Order Utils implemented"
    This reverts commit d4cb8c52e05887f47729c0b5eb3d383e0e1db155.

--- consumer/CMakeFiles/consumer.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libconsumer.a"
-)
--- utils/message_types.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-struct OrderMessage {
-    std::uint64_t order_id;
-    int reps;
-    int time_prd;
-    std::string text;
-}; 
